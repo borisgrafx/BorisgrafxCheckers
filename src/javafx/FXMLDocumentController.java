6668fae6d75322793c0eaa3695e056157bc16023
borisgrafx <boralbel@mail.ru>
2020-08-16 20:20:22 +0300

    v 1.2 of my checkers / English draughts. Code improvements and new tests added

--- src/javafx/FXMLDocumentController.java
+++ src/javafx/FXMLDocumentController.java
@@ -6,24 +6,43 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FXMLDocumentController implements Initializable {
-    @FXML public Button button1;
-    @FXML public Button button2;
-    @FXML public Button button3;
-    @FXML public Button button4;
-    @FXML public Button button5;
-    @FXML public Button button6; @FXML public Button button7; @FXML public Button button8; @FXML public Button button9; @FXML public Button button10;
-    @FXML public Button button11; @FXML public Button button12; @FXML public Button button13; @FXML public Button button14; @FXML public Button button15;
-    @FXML public Button button16; @FXML public Button button17; @FXML public Button button18; @FXML public Button button19; @FXML public Button button20;
-    @FXML public Button button21; @FXML public Button button22; @FXML public Button button23; @FXML public Button button24; @FXML public Button button25;
-    @FXML public Button button26; @FXML public Button button27; @FXML public Button button28; @FXML public Button button29; @FXML public Button button30;
-    @FXML public Button button31; @FXML public Button button32;
-
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+    public Button button10;
+    public Button button11;
+    public Button button12;
+    public Button button13;
+    public Button button14;
+    public Button button15;
+    public Button button16;
+    public Button button17;
+    public Button button18;
+    public Button button19;
+    public Button button20;
+    public Button button21;
+    public Button button22;
+    public Button button23;
+    public Button button24;
+    public Button button25;
+    public Button button26;
+    public Button button27;
+    public Button button28;
+    public Button button29;
+    public Button button30;
+    public Button button31;
+    public Button button32;
 
     private int id;
     private int previd;
@@ -34,6 +53,37 @@ public class FXMLDocumentController implements Initializable {
     private boolean transBig = false;
 
     //event.getSource возвращает строку Button[id=button57, styleClass=button]'' для нажатой кнопки
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttons = new Button[]{button1, button2, button3, button4, button5, button6, button7, button8,
+                button9, button10, button11, button12, button13, button14, button15, button16, button17, button18,
+                button19, button20, button21, button22, button23, button24, button25, button26, button27, button28,
+                button29, button30, button31, button32};
+        checkers = new Checker[32];
+        newGame();
+    }
+
+    public void newGame() {
+        for (int i = 0; i < 12; i++) {
+            checkers[i] = new Checker('b', 'g');
+        }
+        for (int i = 12; i < 20; i++) {
+            checkers[i] = new Checker('e', 'g');
+        }
+        for (int i = 20; i < 32; i++) {
+            checkers[i] = new Checker('w', 'g');
+        }
+        clearColors();
+        id = 0;
+        previd = 1;
+        for (int i = 0; i < 12; i++)
+            imageChanger(i, 'b');
+        for (int i = 12; i < 20; i++)
+            buttons[i].setGraphic(new ImageView());
+        for (int i = 20; i < 32; i++)
+            imageChanger(i, 'w');
+        blackMove = true;
+    }
 
     @FXML
     private void buttonAction(ActionEvent event) {
@@ -44,14 +94,6 @@ public class FXMLDocumentController implements Initializable {
                 id = id * 10 + Character.getNumericValue(source.charAt(i));
         }
         id--;
-        boss();
-        //Жёлтый #FFFF00
-        //Красный #FF0000
-        //Синий #2E9AFE
-        //Зелёный дефолт #006400
-    }
-
-    public void boss() {
         if (killStreak && checkers[id].getColor() != 'y')
             id = previd;
         if (checkers[id].getColor() == 'y') {
@@ -88,11 +130,15 @@ public class FXMLDocumentController implements Initializable {
             }
         }
         previd = id;
+        //Жёлтый #FFFF00
+        //Красный #FF0000
+        //Синий #2E9AFE
+        //Зелёный дефолт #006400
     }
 
     //shouldEat: 'n' = no; 's' = should; 'm' = must
     //'n' - только ходит; 'm' - только ест; 's' - ходит и, если может, ест
-    public void chooser( char shouldEat) {
+    public void chooser(char shouldEat) {
         boolean idsorter;
         if (shouldEat != 'm') {
             if (blackMove || checkers[id].getWhChk() == 'x' || checkers[id].getWhChk() == 'c') {
@@ -159,7 +205,7 @@ public class FXMLDocumentController implements Initializable {
                         && checkers[id + 7].getWhChk() == 'e' && (checkers[id + pluser].getWhChk() == c1
                         || checkers[id + pluser].getWhChk() == c2)) {
                     recoloriser(id + pluser, 'b');
-                    recoloriser(id + 7,  'y');
+                    recoloriser(id + 7, 'y');
                 }
             }
             if (!blackMove || checkers[id].getWhChk() == 'x' || checkers[id].getWhChk() == 'c') {
@@ -225,75 +271,13 @@ public class FXMLDocumentController implements Initializable {
         imageChanger(previd, 'n');
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        buttons = new Button[]{button1, button2, button3, button4, button5, button6, button7, button8,
-                button9, button10, button11, button12, button13, button14, button15, button16, button17, button18,
-                button19, button20, button21, button22, button23, button24, button25, button26, button27, button28,
-                button29, button30, button31, button32};
-        checkers = new Checker[32];
-        newGame();
-    }
-
-    public void newGame() {
-
-        for (int i = 0; i < 12; i++) {
-            checkers[i] = new Checker('b', 'g');
-        }
-        for (int i = 12; i < 20; i++) {
-            checkers[i] = new Checker('e', 'g');
-        }
-        for (int i = 20; i < 32; i++) {
-            checkers[i] = new Checker('w', 'g');
-        }
-        clearColors();
-        id = 0;
-        previd = 1;
-        for (int i = 0; i < 12; i++)
-            imageChanger(i, 'b');
-        for (int i = 12; i < 20; i++)
-            buttons[i].setGraphic(new ImageView());
-        for (int i = 20; i < 32; i++)
-            imageChanger(i, 'w');
-        blackMove = true;
-    }
-
-    public void myWorld(char[] checko){
-        Button button1 = new Button();
-        Button button2 = new Button();
-        Button button3 = new Button();
-        Button button4 = new Button();
-        Button button5 = new Button();
-        Button button6 = new Button();  Button button7 = new Button();  Button button8 = new Button();  Button button9 = new Button();  Button button10 = new Button();
-        Button button11 = new Button(); Button button12 = new Button();  Button button13 = new Button();  Button button14 = new Button(); Button button15 = new Button();
-        Button button16 = new Button(); Button button17 = new Button();  Button button18 = new Button();  Button button19 = new Button();  Button button20 = new Button();
-        Button button21 = new Button();  Button button22 = new Button();  Button button23 = new Button(); Button button24 = new Button();  Button button25 = new Button();
-        Button button26 = new Button();  Button button27 = new Button();  Button button28 = new Button();  Button button29 = new Button();  Button button30 = new Button();
-        Button button31 = new Button();  Button button32 = new Button();
-        buttons = new Button[]{button1, button2, button3, button4, button5, button6, button7, button8,
-                button9, button10, button11, button12, button13, button14, button15, button16, button17, button18,
-                button19, button20, button21, button22, button23, button24, button25, button26, button27, button28,
-                button29, button30, button31, button32};
-        checkers = new Checker[32];
-        newGame();
-        for (int i = 0; i < checko.length; i++) {
-            checkers[i] = new Checker(checko[i], 'g');
-        }
-        for (int i = checko.length; i < 32; i++) {
-            checkers[i] = new Checker('e', 'g');
-        }
-        for (int i = 0; i < checko.length; i++) {
-            checkers[i].setWhChk(checko[i]);
-        }
-    }
-
-    void clearColors(){
+    void clearColors() {
         for (int i = 0; i < 32; i++) {
             recoloriser(i, 'g');
         }
     }
 
-    void recoloriser(int checkerNum, char newColor){
+    void recoloriser(int checkerNum, char newColor) {
         checkers[checkerNum].setColor(newColor);
         switch (newColor) {
             case 'b':
@@ -311,7 +295,7 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
-    void imageChanger(int checkerNum, char name){
+    void imageChanger(int checkerNum, char name) {
         if (name != 'n') {
             InputStream input = getClass().getResourceAsStream(name + ".png");
             Image image = new Image(input);
@@ -325,10 +309,68 @@ public class FXMLDocumentController implements Initializable {
 
     }
 
+    //Далее идут методы, связанные с тестированием
+
+    public void myWorld(char[] checko) {
+        Button button1 = new Button();
+        Button button2 = new Button();
+        Button button3 = new Button();
+        Button button4 = new Button();
+        Button button5 = new Button();
+        Button button6 = new Button();
+        Button button7 = new Button();
+        Button button8 = new Button();
+        Button button9 = new Button();
+        Button button10 = new Button();
+        Button button11 = new Button();
+        Button button12 = new Button();
+        Button button13 = new Button();
+        Button button14 = new Button();
+        Button button15 = new Button();
+        Button button16 = new Button();
+        Button button17 = new Button();
+        Button button18 = new Button();
+        Button button19 = new Button();
+        Button button20 = new Button();
+        Button button21 = new Button();
+        Button button22 = new Button();
+        Button button23 = new Button();
+        Button button24 = new Button();
+        Button button25 = new Button();
+        Button button26 = new Button();
+        Button button27 = new Button();
+        Button button28 = new Button();
+        Button button29 = new Button();
+        Button button30 = new Button();
+        Button button31 = new Button();
+        Button button32 = new Button();
+        buttons = new Button[]{button1, button2, button3, button4, button5, button6, button7, button8,
+                button9, button10, button11, button12, button13, button14, button15, button16, button17, button18,
+                button19, button20, button21, button22, button23, button24, button25, button26, button27, button28,
+                button29, button30, button31, button32};
+        checkers = new Checker[32];
+        newGame();
+        for (int i = 0; i < checko.length; i++) {
+            checkers[i] = new Checker(checko[i], 'g');
+        }
+        for (int i = checko.length; i < 32; i++) {
+            checkers[i] = new Checker('e', 'g');
+        }
+        for (int i = 0; i < checko.length; i++) {
+            checkers[i].setWhChk(checko[i]);
+        }
+    }
+
     public char[] getCheckers() {
         char[] whoIsWho = new char[32];
         for (int i = 0; i < 32; i++)
             whoIsWho[i] = checkers[i].getWhChk();
         return whoIsWho;
+    }
+
+    void setId(int setCur, int setPrev, boolean whoseMove) {
+        id = setCur;
+        previd = setPrev;
+        blackMove = whoseMove;
     }
 }
