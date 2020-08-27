ffa9b79bfc76e688b408280204294b8c538e364e
borisgrafx <boralbel@mail.ru>
2020-08-27 11:05:42 +0300

    v 1.3 (icon) of my checkers / English draughts. Code improvements and icon added.

--- src/javafx/Main.java
+++ src/javafx/Main.java
@@ -4,12 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("checkas.png")));
         primaryStage.setTitle("Englishmen's Draughts");
         primaryStage.setScene(new Scene(root, 400, 480));
         primaryStage.setResizable(false);
